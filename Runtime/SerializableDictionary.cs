ea99314d6e05291dd124a3ecca8e73e0b8c69e40
Strom <stromkuo@gmail.com>
2021-09-27 19:01:03 +0800

    Removed obsolete key-value fields in SerializableDictionary

--- Runtime/SerializableDictionary.cs
+++ Runtime/SerializableDictionary.cs
@@ -7,44 +7,22 @@ namespace SKUnityToolkit.SerializableDictionary
     [Serializable]
     public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
     {
-        [Obsolete]
-        [SerializeField]
-        TKey[] m_keys;
-
-        [Obsolete]
-        [SerializeField]
-        TValue[] m_values;
-
         [SerializeField]
         SerializableKeyValuePair<TKey, TValue>[] m_pairs;
 
         void ISerializationCallbackReceiver.OnAfterDeserialize()
         {
-            if (m_keys != null && m_values != null && m_keys.Length == m_values.Length && m_keys.Length != 0)
+            if (this.m_pairs != null)
             {
                 this.Clear();
-                for (int i = 0; i < m_keys.Length; ++i)
+                foreach (var pair in this.m_pairs)
                 {
-                    this[m_keys[i]] = m_values[i];
-                }
-
-                m_keys = null;
-                m_values = null;
-            }
-            else
-            {
-                if (this.m_pairs != null)
-                {
-                    this.Clear();
-                    foreach (var pair in m_pairs)
+                    if (!this.ContainsKey(pair.Key))
                     {
-                        if (!this.ContainsKey(pair.Key))
-                        {
-                            this[pair.Key] = pair.Value;
-                        }
+                        this[pair.Key] = pair.Value;
                     }
-                    this.m_pairs = null;
                 }
+                this.m_pairs = null;
             }
         }
 
@@ -52,13 +30,12 @@ namespace SKUnityToolkit.SerializableDictionary
         {
             int n = this.Count;
 
-            m_pairs = new SerializableKeyValuePair<TKey, TValue>[n];
+            this.m_pairs = new SerializableKeyValuePair<TKey, TValue>[n];
 
             int i = 0;
             foreach (var pair in this)
             {
-
-                m_pairs[i++] = new SerializableKeyValuePair<TKey, TValue>(pair.Key, pair.Value);
+                this.m_pairs[i++] = new SerializableKeyValuePair<TKey, TValue>(pair.Key, pair.Value);
             }
         }
     }
